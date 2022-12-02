df082bddd36b52ea427b97463bb9596218bc819c
Sungho Park <nonckk@gmail.com>
2022-12-02 17:34:19 +0900

    Update sub1.js
    사이드 바 사진 선택 시 fadeIn(500)효과 추가

--- js/sub1.js
+++ js/sub1.js
@@ -1,33 +1,36 @@
 $(document).ready(function () {
+  $("#sliderbox li:first-child").click(function () {
+    $("#sale_img")
+      .attr("src", "Portfolio_1/sub_page_img/sand_1.jpg")
+      .hide()
+      .fadeIn(500);
+  });
 
-    $("#sliderbox li:first-child").click(function () {
+  $("#sliderbox li:nth-child(2)").click(function () {
+    $("#sale_img")
+      .attr("src", "Portfolio_1/sub_page_img/sand_2.jpg")
+      .hide()
+      .fadeIn(500);
+  });
+  $("#sliderbox li:nth-child(3)").click(function () {
+    $("#sale_img")
+      .attr("src", "Portfolio_1/sub_page_img/sand_3.jpg")
+      .hide()
+      .fadeIn(500);
+  });
+  $("#sliderbox li:nth-child(4)").click(function () {
+    $("#sale_img")
+      .attr("src", "Portfolio_1/sub_page_img/sand_4.jpg")
+      .hide() //fadein()효과를 보여주기 위해 숨김처리
+      .fadeIn(); //fadeIn()
+  });
 
-        $("#sale_img").attr("src", "Portfolio_1/sub_page_img/sand_1.jpg");
+  // 사이드 이미지 클릭 시 sale이미지(큰사진)의 src를
+  // 해당이미지로 변경하여 보여지게하는 코드
 
-    });
-
-    $("#sliderbox li:nth-child(2)").click(function () {
-
-        $("#sale_img").attr("src", "Portfolio_1/sub_page_img/sand_2.jpg");
-
-    });
-    $("#sliderbox li:nth-child(3)").click(function () {
-
-        $("#sale_img").attr("src", "Portfolio_1/sub_page_img/sand_3.jpg");
-
-    });
-    $("#sliderbox li:nth-child(4)").click(function () {
-
-        $("#sale_img").attr("src", "Portfolio_1/sub_page_img/sand_4.jpg");
-
-    });
-    // 사이드 이미지 클릭 시 sale이미지(큰사진)의 src를
-    // 해당이미지로 변경하여 보여지게하는 코드
-
-    $(".aaa").click(function () {
-        $(".aaa").css("border", "none");
-        $(this).css("border", "3px solid lightgray");
-    });
-    // 이미지 클릭 시 테두리 추가 코드
-
+  $(".aaa").click(function () {
+    $(".aaa").css("border", "none");
+    $(this).css("border", "3px solid lightgray");
+  });
+  // 이미지 클릭 시 테두리 추가 코드
 });
